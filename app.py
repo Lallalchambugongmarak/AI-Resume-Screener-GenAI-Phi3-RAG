1c93b2deb9fe799a88f2223ae3eb2f899ebdd570
Lallal Marak <120651491+Lallalchambugongmarak@users.noreply.github.com>
2026-09-19 23:21:44 +0530

    app.py

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ import gradio as gr, PyPDF2, os, re
 
 from sklearn.metrics.pairwise import cosine_similarity
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
 SKILL_BANK = ["Python", "PyTorch", "TensorFlow", "Machine Learning", "Deep Learning",
               "LLM", "Llama-3", "LoRA", "PEFT", "Transformers", "RAG", "Qdrant",
               "FastAPI", "Gradio", "AI Agents", "Colab", "SQL", "Docker", "Kubernetes"]
